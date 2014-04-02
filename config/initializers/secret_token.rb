45604da15a5244628d66573331c9bab1a7399d1d
fo95116 <fo95116@gmail.com>
2014-04-02 13:38:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pineapple::Application.config.secret_token = '477321cac2998b312493f38574a308f4c098156c70c2e4e37f3e5235f030c57b5b1a637b6143fefc24eaca1d119d343ac321e6bc4576bc8d41906c08ee62b619'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pineapple::Application.config.session_store :cookie_store, key: '_pineapple_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pineapple::Application.config.session_store :active_record_store
